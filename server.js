00e21985a1c8c6a9143ca837596d1f05266738d8
Arya <aryaamoriya@gmail.com>
2026-05-09 10:17:54 +0530

    Configured cloud MySQL database

--- server.js
+++ server.js
@@ -27,6 +27,28 @@ db.getConnection()
 
 const PORT = process.env.PORT || 5000;
 
+const createTable = async () => {
+    try {
+
+        await db.execute(`
+            CREATE TABLE IF NOT EXISTS schools (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                address VARCHAR(255) NOT NULL,
+                latitude FLOAT NOT NULL,
+                longitude FLOAT NOT NULL
+            )
+        `);
+
+        console.log("Schools table ready");
+
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+createTable();
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
